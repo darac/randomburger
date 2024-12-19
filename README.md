db45443012fd80239b05ebc484482885ec001676
darac <darac@darac.org.uk>
2024-12-19 08:22:25 +0000

    feat: Add tests

---
+++ README.md
@@ -0,0 +1,45 @@
+# RandomBurger
+
+Based on a stupid idea, prints out a list of "layers" to make a random burger.
+
+Ingredients are printed top down, so a [https://en.wikipedia.org/wiki/Big_Mac](Big Mac)
+would appear as:
+
+```text
+Sesame Seed Bun
+100% Beef Patty
+Pickled Gherkins
+Iceberg Lettuce
+Sliced Onions
+"Special" Sauce
+Sesame Seed Bun
+100% Beef Patty
+American/Processed Cheese
+Iceberg Lettuce
+Sliced Onions
+"Special" Sauce
+Sesame Seed Bun
+```
+
+Some thought has been put into ensuring that the burger has some structure (unless
+`--gowild` is specified); for example, a 1-layer burger will just be a plain patty,
+while a 2-layer burger will be a patty on some sort of bread, and a 3-layer burger is
+basically a sandwich (bread / burger / bread).
+
+There is no guarantee that the resulting burger actually _tastes_ any good.
+
+Arguments about whether the lettuce goes on top or beneath may be raised as pull requests. ☺
+
+## Usage
+
+```text
+usage: randomburger.py [-h] [--layers N] [--vegetarian] [--gowild]
+
+Make some random burgers
+
+options:
+  -h, --help    show this help message and exit
+  --layers N    How many Layers should the burger consist of?
+  --vegetarian  Only use non-meat patties
+  --gowild      Random number of Layers
+```
--- randomburger.py
+++ randomburger.py
@@ -1,7 +1,11 @@
 #!python3
 
+from __future__ import annotations
+
 import argparse
 import random
+
+# region Food Selection
 
 breads = [
     "Sesame Seed Bun",
@@ -10,7 +14,7 @@ breads = [
     "Flatbread",
     "Sliced White",
     "Croissant",
-    "Jam Doughnut"
+    "Jam Doughnut",
 ]
 
 meats = [
@@ -24,14 +28,10 @@ meats = [
     "Pulled Pork",
     "Pulled Turkey",
     "Turducken",
-    "Reindeer Patty"
+    "Reindeer Patty",
 ]
 
-nonmeats = [
-    "Tofu Burger",
-    "Plant-Based 'Beef' Burger",
-    "Nut Burger"
-]
+nonmeats = ["Tofu Burger", "Plant-Based 'Beef' Burger", "Nut Burger"]
 
 cheeses = [
     "American/Processed Cheese",
@@ -39,58 +39,78 @@ cheeses = [
     "Red Leicester",
     "Blue Stilton",
     "Mozzarella",
-    "Feta",
-    "Norwegian Brunost"
+    "Crumbled Feta",
+    "Norwegian Brunost",
 ]
 
 toppings = [
     "Smoked Bacon",
     "Tortilla Chips",
     "Crispy Onions",
-    "Gucamole",
+    "Guacamole",
     "Jalapenos",
-    "Black Olives"
+    "Black Olives",
 ]
 
 sauces = [
     "Burger Sauce",
+    '"Special" Sauce',
     "BBQ Sauce",
     "Mayonnaise",
     "Garlic Mayonnaise",
     "Creamy Dijon Mustard",
     "Tartare Sauce",
-    "Sriracha Sauce"
+    "Sriracha Sauce",
 ]
 
-leaves = [
-    "Iceberg Lettuce",
-    "Rocket",
-    "Curly Kale",
-    "Sliced Sprouts",
-    "Cabbage"
-]
+leaves = ["Iceberg Lettuce", "Rocket", "Curly Kale", "Sliced Sprouts", "Cabbage"]
 
 vegetables = [
     "Sliced Tomato",
-    "Mushroom",
+    "Field Mushroom",
     "Sliced Cucumber",
     "Sliced Bell Pepper",
-    "Sliced Courgette"
+    "Sliced Courgette",
+    "Sliced Onion",
+    "Sliced Red Onion",
 ]
 
+pickles = [
+    "Pickled Gherkins",
+    "Pickled Beetroots",
+    "Pickled Celeriac",
+    "Sweet-pickled Red Onion",
+]
 
-def add(population):
-    return random.choices(population, k=1, weights=list(reversed(range(1, len(population) + 1))))[0]
+# endregion
 
 
-def random_burger(layers=None, vegetarian=False, go_wild=False):
-    if vegetarian:
-        patty = meats
-    else:
-        patty = nonmeats
-    if not go_wild and (layers is None or layers < 10):
-        if layers is None:
-            layers = 7
+def add[T](population: list[T]) -> T:
+    """Return an increasingly unlikely option from the list."""
+    return random.choices(
+        population,
+        k=1,
+        weights=list(reversed(range(1, len(population) + 1))),
+    )[0]
+
+
+def test_add():
+    str_list = ["one", "two", "three"]
+
+    assert add(str_list) in str_list
+
+    list_list = [str_list, str_list, str_list]
+
+    assert add(list_list) in list_list
+
+
+def random_burger(
+    layers: int | None = None,
+    vegetarian: bool = False,
+    go_wild: bool = False,
+) -> list[str]:
+    patty = nonmeats if vegetarian else nonmeats + meats
+    if not go_wild:
         ingredients = {
             1: [patty],
             2: [patty, breads],
@@ -100,29 +120,92 @@ def random_burger(layers=None, vegetarian=False, go_wild=False):
             6: [breads, leaves, vegetables, cheeses, patty, breads],
             7: [breads, leaves, sauces, vegetables, cheeses, patty, breads],
             8: [breads, toppings, sauces, vegetables, patty, cheeses, patty, breads],
-            9: [breads, leaves, sauces, vegetables, patty, vegetables, cheeses, patty, breads]
-        }[layers]
+            9: [
+                breads,
+                leaves,
+                sauces,
+                vegetables,
+                patty,
+                vegetables,
+                cheeses,
+                patty,
+                breads,
+            ],
+            # Attempt a Big Mac
+            13: [
+                breads,
+                meats,
+                pickles,
+                leaves,
+                vegetables,
+                sauces,
+                breads,
+                meats,
+                cheeses,
+                leaves,
+                vegetables,
+                sauces,
+                breads,
+            ],
+        }[layers or 7]
     else:
         ingredients = []
         if layers is None:
             layers = random.randint(1, 25)
-        for i in range(layers):
+        for _ in range(layers):
             if vegetarian:
-                ingredients.append(add([breads, nonmeats, cheeses, toppings, sauces, leaves, vegetables]))
+                ingredients.append(
+                    add(
+                        [
+                            breads,
+                            nonmeats,
+                            cheeses,
+                            toppings,
+                            sauces,
+                            leaves,
+                            vegetables,
+                        ],
+                    ),
+                )
             else:
-                ingredients.append(add([breads, meats, nonmeats, cheeses, toppings, sauces, leaves, vegetables]))
+                ingredients.append(
+                    add(
+                        [
+                            breads,
+                            meats,
+                            nonmeats,
+                            cheeses,
+                            toppings,
+                            sauces,
+                            leaves,
+                            vegetables,
+                        ],
+                    ),
+                )
 
-    for i in ingredients:
-        print(add(i))
+    return [add(_) for _ in ingredients]
 
 
-if __name__ == '__main__':
+def test_random_burger():
+    assert random_burger(1)[0] in meats + nonmeats
+    assert random_burger(1, vegetarian=True)[0] in nonmeats
+    assert len(random_burger(go_wild=True)) in range(1, 26)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Make some random burgers")
-    parser.add_argument('--layers', metavar='N', type=int)
-    parser.add_argument('--vegetarian', action='store_true')
-    parser.add_argument('--gowild', action='store_true')
+    parser.add_argument(
+        "--layers",
+        metavar="N",
+        type=int,
+        help="How many Layers should the burger consist of?",
+    )
+    parser.add_argument(
+        "--vegetarian", action="store_true", help="Only use non-meat patties"
+    )
+    parser.add_argument("--gowild", action="store_true", help="Random number of Layers")
 
     args = parser.parse_args()
 
     random.seed()
-    random_burger(args.layers, args.vegetarian, args.gowild)
+    print("\n".join(random_burger(args.layers, args.vegetarian, args.gowild)))
